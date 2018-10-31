54fa44e5f6abb5f96fc5ddbadf1351fac24f1bcc
Julian Civau <jcivau@gmail.com>
2018-10-31 18:14:55 -0300

    No inserta los datos pero los lee

--- add.php
+++ add.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>
-<html lang="es">  
-   <head>    
-      <title>TFI Julian Civau</title>    
-      <meta charset="UTF-8">
-      <meta name="title" content="Título de la WEB">
-      <meta name="description" content="Descripción de la WEB">    
-      <!--<link href="http://dominio.com/hoja-de-estilos.css" rel="stylesheet" type="text/css"/>-->
-   </head> 
-
-   <body>
-   <h1>TFI Julian Civau</h1>
-   <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa sint nesciunt doloremque, ratione officia, voluptate placeat recusandae, totam libero explicabo magnam labore a delectus commodi dolor iusto quae! Totam, officiis.</p>
-
 <?php
    include("connect.php");
       
@@ -20,24 +6,14 @@
    /*$temp1=$_POST["temp1"];
    $hum1=$_POST["hum1"];*/
 
-   $temp1="15";
-   $hum1="15";
 
-   $insertar = "INSERT INTO `tempLog` (`temperature`, `humidity`) VALUES ('".$temp1."','".$hum1."')";
+   $insertar = "INSERT INTO `tempLog` (`temperature`, `humidity`) VALUES ('".$_GET["temp1"]."', '".$_GET["hum1"]."')";
 
    if ($con->query($insertar) === FALSE) {
             echo "Error: " . $insertar . "<br>" . $con->error;
          }
-      
-   //mysql_query($query,$link);
-   //mysql_close($link);
+
    $con->close();
 
-   //header("Location: index.php");
-   printf("HOLA!")
+   header("Location: index.php");
 ?>
-   <a href="index.php">VOLVER!</a>
-
-   </body>
-
-</html>
---
+++ tfi_ethernet_temp/tfi_ethernet_temp.ino
@@ -0,0 +1,97 @@
+#include <DHT.h>
+#include <Ethernet.h>
+#include <SPI.h>
+
+byte mac[] = { 0x00, 0xAA, 0xBB, 0xCC, 0xDE, 0x01 }; // RESERVED MAC ADDRESS
+EthernetClient client;
+
+#define DHTPIN 2 // SENSOR PIN
+#define DHTTYPE DHT11 // SENSOR TYPE - THE ADAFRUIT LIBRARY OFFERS SUPPORT FOR MORE MODELS
+DHT dht(DHTPIN, DHTTYPE);
+
+long previousMillis = 0;
+unsigned long currentMillis = 0;
+long interval = 50000; // READING INTERVAL
+
+int ledAzul = 7;
+
+int t = 0;	// TEMPERATURE VAR
+int h = 0;	// HUMIDITY VAR
+String data;
+
+void setup() { 
+	Serial.begin(115200);
+  pinMode(ledAzul,OUTPUT);
+
+	if (Ethernet.begin(mac) == 0) {
+		Serial.println("Failed to configure Ethernet using DHCP"); 
+	}
+
+	dht.begin(); 
+	delay(10000); // GIVE THE SENSOR SOME TIME TO START
+
+	h = (int) dht.readHumidity(); 
+	t = (int) dht.readTemperature(); 
+
+	data = "";
+}
+
+void loop(){
+  
+  //Serial.print("hola");
+  
+	currentMillis = millis();
+	if(currentMillis - previousMillis > interval) { // READ ONLY ONCE PER INTERVAL
+		previousMillis = currentMillis;
+		h = (int) dht.readHumidity();
+		t = (int) dht.readTemperature();
+	}
+
+  Serial.print("Temp: ");
+  Serial.println(t);
+  Serial.print("Humedad: ");
+  Serial.println(h);
+	data = "temp1="  + (String)t + "&&hum1=" + (String)h;
+  
+
+	/*if (client.connect("www.tfi.plazacontenidos.com",80)) { // REPLACE WITH YOUR SERVER ADDRESS
+		client.println("POST /add.php HTTP/1.1"); 
+		client.println("Host: tfi.plazacontenidos.com"); // SERVER ADDRESS HERE TOO
+		client.println("Content-Type: application/x-www-form-urlencoded"); 
+		client.print("Content-Length: "); 
+		client.println(data.length()); 
+		client.println(); 
+		client.print(data);
+    Serial.println(data.length());
+    Serial.println("Paso el insert");
+	}*/
+
+  if (client.connect("www.tfi.plazacontenidos.com",80)) {
+    
+    client.print( "/add.php?");
+    client.print(data);
+
+    client.print( " HTTP/1.0");
+    client.print(" User-Agent: Arduino 1.0");
+    client.println();
+
+digitalWrite(ledAzul, HIGH);
+Serial.println(data);
+Serial.println("Estoy adentro");
+  }
+  
+  
+
+	if (client.connected()) { 
+		client.stop();	// DISCONNECT FROM THE SERVER
+	}
+
+  Serial.println(Ethernet.localIP());
+  
+	delay(10000); // WAIT FIVE MINUTES BEFORE SENDING AGAIN
+  /*digitalWrite(ledAzul, LOW);
+  delay(1000);*/
+}
+
+
+
